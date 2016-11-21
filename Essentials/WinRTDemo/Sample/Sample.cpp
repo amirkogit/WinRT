80cac201fbf9e9942e8e21ed7e834af41afce1d7
amirkogit <amirkomail@gmail.com>
2016-11-21 10:36:02 -0500

    Creating a new class Implements using variadic template concept to streamline the management of COM interface pointers

--- Essentials/WinRTDemo/Sample/Sample.cpp
+++ Essentials/WinRTDemo/Sample/Sample.cpp
@@ -1,7 +1,79 @@
 #include "Precompiled.h"
 #include <windows.h>
+#include <type_traits>
 
 using Microsoft::WRL::ComPtr;
+
+template<typename ... Interfaces>
+class __declspec(novtable) Implements : public Intefaces ...
+{
+    long m_references = 1;
+
+    template<int = 0> // placeholder template argument
+    void * QueryInterface(GUID const &) noexcept
+    {
+        return nullptr;
+    }
+
+    template<typename First, typename ... Rest>
+    void * QueryInteface(GUID const & id) noexcept
+    {
+        if (id == __uuidof(First))
+        {
+            return static_cast<First *>(this);
+        }
+
+        return QueryInterface<Rest ...>(id);
+    }
+
+    template<typename First, typename ... Rest>
+    void * BaseQueryInterface(GUID const & id) noexcept
+    {
+        if (id == __uuidof(First) ||
+            id == __uuidof(IUnknown) ||
+            (std::is_base_of<IInspectable, First>::value && id == __uuidof(IInspectable)))
+        {
+            return static_cast<First *>(this);
+        }
+
+        return QueryInterface<Rest ...>(id);
+    }
+
+protected:
+    virtual ~Implements() noexcept {}
+
+public:
+    unsigned long __stdcall AddRef() noexcept
+    {
+        return InterlockedIncrement(&m_references);
+    }
+
+    unsigned long __stdcall Release() noexcept
+    {
+        unsigned long const count = InterlockedDecrement(&m_references);
+
+        if (0 == count)
+        {
+            delete this;
+        }
+
+        return count;
+    }
+
+    HRESULT __stdcall QueryInterface(GUID const & id,
+        void ** object) noexcept
+    {
+        *object = BaseQueryInteface<Interfaces ...>(id);
+
+        if (!*object)
+        {
+            return E_NOINTERFACE;
+        }
+
+        static_cast<IUnknown *>(*object)->AddRef();
+        return S_OK;
+    }
+};
 
 struct __declspec(uuid("3a8af621-4232-40dd-9582-4379030eb576")) __declspec(novtable)
 IHen : IUnknown
