b358a3102bdcafee504b9f94b4287963c305c14b
amirkogit <amirkomail@gmail.com>
2016-11-30 11:51:06 -0500

    First take with the implementation of a component

--- Essentials/WinRTDemo/Component/Component.cpp
+++ Essentials/WinRTDemo/Component/Component.cpp
@@ -1,1 +1,11 @@
 #include "Precompiled.h"
+#include "Component.h"
+#include "Implements.h"
+
+struct Hen : Implements<ABI::Component::IHen>
+{
+    HRESULT __stdcall Cluck() noexcept
+    {
+        return S_OK;
+    }
+};
