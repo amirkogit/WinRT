4bb744b43e3d4fd66c27cc8a98a86deaf86537e7
amirkogit <amirkomail@gmail.com>
2016-11-30 13:44:15 -0500

    Supporting activation

--- Essentials/WinRTDemo/Component/Component.cpp
+++ Essentials/WinRTDemo/Component/Component.cpp
@@ -1,11 +1,36 @@
 #include "Precompiled.h"
 #include "Component.h"
 #include "Implements.h"
+#include <activation.h>
+
+static long s_serverLock;
 
 struct Hen : Implements<ABI::Component::IHen>
 {
+    Hen() noexcept
+    {
+        InterlockedIncrement(&s_serverLock);
+    }
+
+    ~Hen() noexcept
+    {
+        InterlockedDecrement(&s_serverLock);
+    }
+
     HRESULT __stdcall Cluck() noexcept
     {
         return S_OK;
     }
 };
+
+// Exported DLL functions implementation
+HRESULT __stdcall DllCanUnloadNow() noexcept
+{
+    return s_serverLock ? S_FALSE : S_OK;
+}
+
+HRESULT __stdcall DllGetActivationFactory(HSTRING classId,
+                                          IActivationFactory ** factory) noexcept
+{
+    return E_NOTIMPL;
+}
