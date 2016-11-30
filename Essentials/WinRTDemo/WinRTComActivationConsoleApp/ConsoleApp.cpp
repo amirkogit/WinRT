2e32448b885e255f4ca170d251a1aacfd3b978af
amirkogit <amirkomail@gmail.com>
2016-11-30 14:30:31 -0500

    Activating ICalendar component and reading year and month from it

--- Essentials/WinRTDemo/WinRTComActivationConsoleApp/ConsoleApp.cpp
+++ Essentials/WinRTDemo/WinRTComActivationConsoleApp/ConsoleApp.cpp
@@ -1,6 +1,39 @@
 #include "Precompiled.h"
+#include <roapi.h>
+#include <wrl.h>
+#include "String.h"
+#include <windows.globalization.h>
+
+#pragma comment(lib, "runtimeobject")
+
+using Microsoft::WRL::ComPtr;
+using namespace ABI::Windows::Globalization;
 
 int main()
 {
+    Check(RoInitialize(RO_INIT_MULTITHREADED));
+
+    StringReference string(L"Windows.Globalization.Calendar");
+
+    ComPtr<IInspectable> inspectable;
+
+    Check(RoActivateInstance(string.Get(),
+                             inspectable.GetAddressOf()));
+
+    ComPtr<ICalendar> calendar;
+
+    Check(inspectable.As(&calendar));
+
+    int year = 0;
+
+    Check(calendar->get_Year(&year));
+
+    String month;
+
+    Check(calendar->MonthAsFullString(month.GetAddressOf()));
+
+    printf("%ls %d\n",
+            Buffer(month),
+            year);
 
 }
--- Essentials/WinRTDemo/WinRTComActivationConsoleApp/Precompiled.h
+++ Essentials/WinRTDemo/WinRTComActivationConsoleApp/Precompiled.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <wrl.h>
+#include <Windows.h>
+#include <stdio.h>
+#include <winstring.h>
+#include <algorithm>
+
+#pragma comment(lib, "runtimeobject")
+
+#ifdef _DEBUG
+    #include <crtdbg.h>
+    #include <stdio.h>
+
+    #define ASSERT _ASSERTE
+    #define VERIFY ASSERT
+    #define VERIFY_(result, expression) ASSERT(result == expression)
+
+    template <typename ... Args>
+    void TRACE(wchar_t const * format, Args ... args)
+    {
+        wchar_t buffer[200];
+        swprintf_s(buffer, format, args ...);
+
+        OutputDebugStringW(buffer);
+    }
+
+#else
+    #define ASSERT __noop
+    #define VERIFY(expression) (expression)
+    #define VERIFY_(result, expression) (expression)
+    #define TRACE __noop
+#endif
+
+
+#include "String.h"
